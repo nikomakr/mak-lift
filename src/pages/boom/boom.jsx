775b2acf8674a455f3adeac3a20e46ede13515c9
makridis nikolas <makridisnikolas@gmail.com>
2024-01-20 20:29:50 +0000

    path updates

--- src/pages/boom/boom.jsx
+++ src/pages/boom/boom.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import boomCalendar from '../../components/boomCalendar/boomCalendar';
+import BoomCalendar from '../../components/boomCalendar/boomCalendar';
 
 function Boom() {
   return (
     <div>
       <h1>Boom Lift Page</h1>
-      <boomCalendar />
+      <BoomCalendar />
     </div>
   );
 }
