efc03920f4d17c2c59b29207b2b6b7a2f24db9bd
the.sholaakanbi <oluwalonshola6@gmail.com>
2024-01-21 10:46:21 +0000

    adding codes

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+
+export default function aboutUs () {
+  return (
+<div className="mt-4 p-5 hart text-white rounded">
+  <h1>About Us</h1>
+  <p>Lorem ipsum...</p>
+</div>
+
+
+  )
+}
