29bf7938adb7313102e4b16ec837241a94d95c3c
CLAmadi <chiamakaamadi93@gmail.com>
2024-01-21 00:20:51 +0000

    Formatting page and commencing form state management

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,48 +1,32 @@
-// function Form (){
-//     return (
-//         <form className="--form-control --card--flex-center --dir-column">
-//         <input 
-//         type="text" 
-//         placeholder="First Name"
-//         name="user_firstname" required/>
-//         <input 
-//         type="text" 
-//         placeholder="Last Name"
-//         name="user_lastname" required/><input 
-//         type="text" 
-//         placeholder="Email"
-//         name="E-mail" required/>
-//         <input 
-//         type="text" 
-//         placeholder="Mobile"
-//         name="Mobile number" required/>
-//         <input 
-//         type="text" 
-//         placeholder="Subject"
-//         name="subject" required/>
-//         <textarea name="message" cols="30" rows="10"></textarea>
-//         <button type='submit' className="--btn --btn-primary">Submit</button>
-//     </form>
-//     )
-// }
-
-// export default Form
 import React from 'react';
+import { useFormik } from 'formik';
 
-const Form = () => {
+function Form () {
+  const formik = useFormik({
+    initialValues:{
+      companyname:'',
+      email:'',
+      givenname:'',
+      surname:'',
+      contact:'',
+      subject:'',
+      message:'',
+    }
+  })
+  console.log('Form values', formik.values)
   return (
     <>
     <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-center' }}>
-      <label htmlFor='company name'>Company Name</label>
-      <input type="text" id='company name' name="company name" required />
+      <label htmlFor='companyname'>Company Name</label>
+      <input type="text" id='companyname' name="companyname" onChange={formik.handleChange} value={formik.values.name} required />
       <label htmlFor='email address'>Email Address</label>
-      <input type="text" id='email' name="E-mail" required />
-      <label htmlFor='Given Name'>Given Name</label>
-      <input type="text" id='given-name' name="given name" required />
-      <label htmlFor='Surname'>Surname</label>
-      <input type="text" id="Surname" name="user_lastname" required />
-      <label htmlFor='contact number'>Contact Number</label>
-      <input type="text" id="Contact number" name="contact number" required />
+      <input type="text" id='email' name="email" required />
+      <label htmlFor='givenname'>Given Name</label>
+      <input type="text" id='givenname' name="givenname" required />
+      <label htmlFor='surname'>Surname</label>
+      <input type="text" id="surname" name="surname" required />
+      <label htmlFor='contact'>Contact Number</label>
+      <input type="text" id="Contact" name="contact" required />
       <label htmlFor='Subject'>Subject</label>
       <input type="text" id="Subject" name="subject" required />
       <label htmlFor='message'>Message</label>
