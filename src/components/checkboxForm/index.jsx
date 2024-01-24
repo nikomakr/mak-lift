8f25373cec958dfc772271a30f82c8f46ac64547
CLAmadi <chiamakaamadi93@gmail.com>
2024-01-24 20:01:20 +0000

    debugging

--- src/components/checkboxForm/index.jsx
+++ src/components/checkboxForm/index.jsx
@@ -21,7 +21,7 @@ function FormikContainer() {
   });
 
   const handleSubmit = (values) => {
-    console.log('Form submitted:', values);
+    console.log('FormikContainer submitted:', values);
   };
 
   return (
@@ -34,56 +34,143 @@ function FormikContainer() {
       validationSchema={validationSchema}
       onSubmit={handleSubmit}
     >
-      <Form>
-        <fieldset className="group">
-          <legend>Fleet Options :</legend>
-          <ul className="checkbox">
-            {checkboxOptions.map(check => (
-              <li key={check.value}>
-                <Field
-                  type="checkbox"
-                  id={check.value}
-                  name="fleetOptions"
-                  value={check.value}
-                />
-                <label htmlFor={check.value}>{check.key}</label>
-              </li>
-            ))}
-          </ul>
-        </fieldset>
+      {() => (
+        <Form>
+          <fieldset className="group">
+            <legend>Fleet Options :</legend>
+            <ul className="checkbox">
+              {checkboxOptions.map(check => (
+                <li key={check.value}>
+                  <Field
+                    key={check.value}
+                    type="checkbox"
+                    id={check.value}
+                    name="fleetOptions"
+                    value={check.value}
+                  />
+                  <label htmlFor={check.value}>{check.key}</label>
+                </li>
+              ))}
+            </ul>
+          </fieldset>
 
-        <Row className="align-items-left">
-          <Col xs="auto">
-            <Form.Label htmlFor="startDate">Start date</Form.Label>
-            <Field
-              type="text"
-              className="mt-0"
-              id="startDate"
-              name="startDate"
-              placeholder="dd-mm-yyy"
-            />
-            <ErrorMessage name="startDate" component="div" className="error" />
-          </Col>
-          <Col xs="auto">
-            <Form.Label htmlFor="endDate">End date</Form.Label>
-            <Field
-              type="text"
-              className="mt-0"
-              id="endDate"
-              name="endDate"
-              placeholder="dd-mm-yyy"
-            />
-            <ErrorMessage name="endDate" component="div" className="error" />
-          </Col>
-        </Row>
-
-        <button type="submit">Submit</button>
-      </Form>
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <label htmlFor="startDate">Start date</label>
+              <Field
+                type="text"
+                className="mt-0"
+                id="startDate"
+                name="startDate"
+                placeholder="dd-mm-yyy"
+              />
+              <ErrorMessage name="startDate" component="div" className="error" />
+            </Col>
+            <Col xs="auto">
+              <label htmlFor="endDate">End date</label>
+              <Field
+                type="text"
+                className="mt-0"
+                id="endDate"
+                name="endDate"
+                placeholder="dd-mm-yyy"
+              />
+              <ErrorMessage name="endDate" component="div" className="error" />
+            </Col>
+          </Row>
+        </Form>
+      )}
     </Formik>
   );
 }
 
 export default FormikContainer;
+
+// import React from 'react';
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import * as Yup from 'yup';
+// import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
+// import "./style.css";
+
+// function FormikContainer() {
+//   const checkboxOptions = [
+//     { key: 'Boom Lift', value: 'cBoomLift' },
+//     { key: 'Scissors Lift', value: 'cScissorsLift' },
+//     { key: 'Tracked Spider Mount', value: 'cTrackedspidermount' },
+//     { key: 'Truck Mount Lift', value: 'cTruckmountLift' },
+//     { key: 'Not sure', value: 'cNotsure' }
+//   ];
+
+//   const validationSchema = Yup.object().shape({
+//     fleetOptions: Yup.array().min(1, 'Please select at least one fleet option.'),
+//     startDate: Yup.string().required('Start date is required.'),
+//     endDate: Yup.string().required('End date is required.')
+//   });
+
+//   const handleSubmit = (values) => {
+//     console.log('FormikContainer submitted:', values);
+//   };
+
+//   return (
+//     <Formik
+//       initialValues={{
+//         fleetOptions: [],
+//         startDate: '',
+//         endDate: ''
+//       }}
+//       validationSchema={validationSchema}
+//       onSubmit={handleSubmit}
+//     >
+//       <Form>
+//         <fieldset className="group">
+//           <legend>Fleet Options :</legend>
+//           <ul className="checkbox">
+//             {checkboxOptions.map(check => (
+//               <li key={check.value}>
+//                 <Field
+//                   key={check.value}
+//                   type="checkbox"
+//                   id={check.value}
+//                   name="fleetOptions"
+//                   value={check.value}
+//                 />
+//                 <label htmlFor={check.value}>{check.key}</label>
+//               </li>
+//             ))}
+//           </ul>
+//         </fieldset>
+
+//         <Row className="align-items-left">
+//           <Col xs="auto">
+//             <label htmlFor="startDate">Start date</label>
+//             <Field
+//               type="text"
+//               className="mt-0"
+//               id="startDate"
+//               name="startDate"
+//               placeholder="dd-mm-yyy"
+//             />
+//             <ErrorMessage name="startDate" component="div" className="error" />
+//           </Col>
+//           <Col xs="auto">
+//             <label htmlFor="endDate">End date</label>
+//             <Field
+//               type="text"
+//               className="mt-0"
+//               id="endDate"
+//               name="endDate"
+//               placeholder="dd-mm-yyy"
+//             />
+//             <ErrorMessage name="endDate" component="div" className="error" />
+//           </Col>
+//         </Row>
+//       </Form>
+//     </Formik>
+//   );
+// }
+
+// export default FormikContainer;
 
 // import React from 'react';
 // import Form from 'react-bootstrap/Form';
@@ -136,7 +223,6 @@ export default FormikContainer;
 //   );
 // }
 
-// export default FormikContainer;  
 
 
 {/* import InputGroup from 'react-bootstrap/InputGroup'; 
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import CheckboxForm from '../checkboxForm';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import "./style.css";
 
 const initialValues = {
@@ -12,11 +14,22 @@ const initialValues = {
   contact: '',
   subject: '',
   message: '',
+  fleetOptions: [],
+        startDate: '',
+        endDate: ''
 };
 
 const onSubmit = values => {
-  console.log('Form data', values);
+  console.log('MyForm data', values);
 };
+
+const checkboxOptions = [
+  { key: 'Boom Lift', value: 'cBoomLift' },
+  { key: 'Scissors Lift', value: 'cScissorsLift' },
+  { key: 'Tracked Spider Mount', value: 'cTrackedspidermount' },
+  { key: 'Truck Mount Lift', value: 'cTruckmountLift' },
+  { key: 'Not sure', value: 'cNotsure' }
+];
 
 const validationSchema = Yup.object({
   companyname: Yup.string().required('Required!'),
@@ -24,6 +37,11 @@ const validationSchema = Yup.object({
   givenname: Yup.string().required('Required'),
   surname: Yup.string().required('Required'),
   contact: Yup.string().required('Required'),
+})
+Yup.object().shape({
+  fleetOptions: Yup.array().min(1, 'Please select at least one fleet option.'),
+  startDate: Yup.string().required('Start date is required.'),
+  endDate: Yup.string().required('End date is required.')
 });
 
 function MyForm() {
@@ -33,48 +51,174 @@ function MyForm() {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      <Form
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'start',
-        }}
-      >
-        <label htmlFor='companyname'>Company Name</label>
-        <Field type='text' id='companyname' name='companyname' />
-        <ErrorMessage name='companyname' component='div' />
+      {() => (
+        <Form
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'start',
+          }}
+        >
+          <label htmlFor='companyname'>Company Name</label>
+          <Field type='text' id='companyname' name='companyname' />
+          <ErrorMessage name='companyname' component='div' />
 
-        <label htmlFor='email'>Email Address</label>
-        <Field type='text' id='email' name='email' />
-        <ErrorMessage name='email' component='div' />
+          <label htmlFor='email'>Email Address</label>
+          <Field type='text' id='email' name='email' />
+          <ErrorMessage name='email' component='div' />
 
-        <label htmlFor='givenname'>Given Name</label>
-        <Field type='text' id='givenname' name='givenname' />
-        <ErrorMessage name='givenname' component='div' />
+          <label htmlFor='givenname'>Given Name</label>
+          <Field type='text' id='givenname' name='givenname' />
+          <ErrorMessage name='givenname' component='div' />
 
-        <label htmlFor='surname'>Surname</label>
-        <Field type='text' id='surname' name='surname' />
-        <ErrorMessage name='surname' component='div' />
+          <label htmlFor='surname'>Surname</label>
+          <Field type='text' id='surname' name='surname' />
+          <ErrorMessage name='surname' component='div' />
 
-        <label htmlFor='contact'>Contact Number</label>
-        <Field type='text' id='contact' name='contact' />
-        <ErrorMessage name='contact' component='div' />
+          <label htmlFor='contact'>Contact Number</label>
+          <Field type='text' id='contact' name='contact' />
+          <ErrorMessage name='contact' component='div' />
 
-        <br />
+          <br />
 
-        <label className='hire' htmlFor='hire'>Looking to hire</label>
-        <CheckboxForm />
+          <label className='hire' htmlFor='hire'>Looking to hire</label>
+          <fieldset className="group">
+            <legend>Fleet Options :</legend>
+            <ul className="checkbox">
+              {checkboxOptions.map(check => (
+                <li key={check.value}>
+                  <Field
+                    key={check.value}
+                    type="checkbox"
+                    id={check.value}
+                    name="fleetOptions"
+                    value={check.value}
+                  />
+                  <label htmlFor={check.value}>{check.key}</label>
+                </li>
+              ))}
+            </ul>
+          </fieldset>
 
-        <label htmlFor='subject'>Subject</label>
-        <Field type='text' id='subject' name='subject' />
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <label htmlFor="startDate">Start date</label>
+              <Field
+                type="text"
+                className="mt-0"
+                id="startDate"
+                name="startDate"
+                placeholder="dd-mm-yyy"
+              />
+              <ErrorMessage name="startDate" component="div" className="error" />
+            </Col>
+            <Col xs="auto">
+              <label htmlFor="endDate">End date</label>
+              <Field
+                type="text"
+                className="mt-0"
+                id="endDate"
+                name="endDate"
+                placeholder="dd-mm-yyy"
+              />
+              <ErrorMessage name="endDate" component="div" className="error" />
+            </Col>
+          </Row>
+          
+          {/* <CheckboxForm /> */}
 
-        <label htmlFor='message'>Message</label>
-        <Field as='textarea' name='message' />
+          <label htmlFor='subject'>Subject</label>
+          <Field type='text' id='subject' name='subject' />
 
-        <button type='submit'>Submit</button>
-      </Form>
+          <label htmlFor='message'>Message</label>
+          <Field as='textarea' name='message' />
+
+          <button type='submit'>Submit</button>
+        </Form>
+      )}
     </Formik>
   );
 }
 
 export default MyForm;
+
+// import React from 'react';
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import * as Yup from 'yup';
+// import CheckboxForm from '../checkboxForm';
+// import "./style.css";
+
+// const initialValues = {
+//   companyname: '',
+//   email: '',
+//   givenname: '',
+//   surname: '',
+//   contact: '',
+//   subject: '',
+//   message: '',
+// };
+
+// const onSubmit = values => {
+//   console.log('MyForm data', values);
+// };
+
+// const validationSchema = Yup.object({
+//   companyname: Yup.string().required('Required!'),
+//   email: Yup.string().email('Invalid email format').required('Required'),
+//   givenname: Yup.string().required('Required'),
+//   surname: Yup.string().required('Required'),
+//   contact: Yup.string().required('Required'),
+// });
+
+// function MyForm() {
+//   return (
+//     <Formik
+//       initialValues={initialValues}
+//       validationSchema={validationSchema}
+//       onSubmit={onSubmit}
+//     >
+//       <Form
+//         style={{
+//           display: 'flex',
+//           flexDirection: 'column',
+//           alignItems: 'start',
+//         }}
+//       >
+//         <label htmlFor='companyname'>Company Name</label>
+//         <Field type='text' id='companyname' name='companyname' />
+//         <ErrorMessage name='companyname' component='div' />
+
+//         <label htmlFor='email'>Email Address</label>
+//         <Field type='text' id='email' name='email' />
+//         <ErrorMessage name='email' component='div' />
+
+//         <label htmlFor='givenname'>Given Name</label>
+//         <Field type='text' id='givenname' name='givenname' />
+//         <ErrorMessage name='givenname' component='div' />
+
+//         <label htmlFor='surname'>Surname</label>
+//         <Field type='text' id='surname' name='surname' />
+//         <ErrorMessage name='surname' component='div' />
+
+//         <label htmlFor='contact'>Contact Number</label>
+//         <Field type='text' id='contact' name='contact' />
+//         <ErrorMessage name='contact' component='div' />
+
+//         <br />
+
+//         <label className='hire' htmlFor='hire'>Looking to hire</label>
+//         <CheckboxForm />
+
+//         <label htmlFor='subject'>Subject</label>
+//         <Field type='text' id='subject' name='subject' />
+
+//         <label htmlFor='message'>Message</label>
+//         <Field as='textarea' name='message' />
+
+//         <button type='submit'>Submit</button>
+//       </Form>
+//     </Formik>
+//   );
+// }
+
+// export default MyForm;
